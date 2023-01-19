df5c70c863c7848e16b2a961f074319a2bf1e802
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-01-19 23:05:48 +0000

    button

--- src/main/java/xyz/amymialee/elegantarmour/mixin/client/ElytraFeatureRendererMixin.java
+++ src/main/java/xyz/amymialee/elegantarmour/mixin/client/ElytraFeatureRendererMixin.java
@@ -1,6 +1,5 @@
 package xyz.amymialee.elegantarmour.mixin.client;
 
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.feature.ElytraFeatureRenderer;
 import net.minecraft.client.render.entity.model.EntityModel;
--- src/main/java/xyz/amymialee/elegantarmour/mixin/client/OptionsScreenMixin.java
+++ src/main/java/xyz/amymialee/elegantarmour/mixin/client/OptionsScreenMixin.java
@@ -1,7 +1,13 @@
 package xyz.amymialee.elegantarmour.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.gui.Element;
+import net.minecraft.client.gui.Selectable;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
+import net.minecraft.client.gui.screen.option.SkinOptionsScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
@@ -9,8 +15,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.amymialee.elegantarmour.client.ElegantOptionsScreen;
 
 @Mixin(OptionsScreen.class)
@@ -21,12 +25,12 @@ public class OptionsScreenMixin extends Screen {
         super(title);
     }
 
-    @Inject(method = "init", at = @At("TAIL"))
-    private void elegantArmour$init(CallbackInfo ci) {
+    @WrapOperation(method = "init", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/option/OptionsScreen;addDrawableChild(Lnet/minecraft/client/gui/Element;)Lnet/minecraft/client/gui/Element;", ordinal = 4))
+    private <T extends Element & Drawable & Selectable> T elegantArmour$thinSkinOptions(OptionsScreen screen, ButtonWidget widget, Operation<T> original) {
         if (this.client == null) {
-            return;
+            return original.call(screen, widget);
         }
-
-        this.addDrawableChild(new ButtonWidget(this.width / 2 - 155 - 30, this.height / 6 + 48 - 6, 20, 20, Text.translatable("options.elegantCustomisation"), button -> this.client.setScreen(new ElegantOptionsScreen(this, this.settings))));
+        this.addDrawableChild(new ButtonWidget(this.width / 2 - 155 + 130, this.height / 6 + 48 - 6, 20, 20, Text.translatable("options.elegantCustomisation"), button -> this.client.setScreen(new ElegantOptionsScreen(this, this.settings))));
+        return original.call(screen, new ButtonWidget(this.width / 2 - 155, this.height / 6 + 48 - 6, 130, 20, Text.translatable("options.skinCustomisation"), button -> this.client.setScreen(new SkinOptionsScreen(this, this.settings))));
     }
 }
