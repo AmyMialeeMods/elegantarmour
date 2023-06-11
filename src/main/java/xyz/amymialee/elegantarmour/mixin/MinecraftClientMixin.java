fdeaf85d8aaf9e272b25032f64770e37f8bad75b
AmyMialee <10999535+AmyMialee@users.noreply.github.com>
2023-06-11 05:42:29 +0100

    always visible

---
+++ src/main/java/xyz/amymialee/elegantarmour/mixin/MinecraftClientMixin.java
@@ -0,0 +1,15 @@
+package xyz.amymialee.elegantarmour.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+    @WrapOperation(method = "handleInputEvents", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/MinecraftClient;isConnectedToServer()Z"))
+    private boolean elegantArmour$allowMenu(MinecraftClient client, Operation<Boolean> operation) {
+        return true;
+    }
+}
